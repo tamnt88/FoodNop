bd810973c5f2a85b8de6e8fffc2899fdfb7ac602
alex <alex.huynh@beyondedge.co>
2026-01-29 17:31:11 +0700

    fix update

--- src/Presentation/Nop.Web/Components/HomepageRecentProductsViewComponent.cs
+++ src/Presentation/Nop.Web/Components/HomepageRecentProductsViewComponent.cs
@@ -40,7 +40,7 @@ public partial class HomepageRecentProductsViewComponent : NopViewComponent
 
         var model = (await _productModelFactory.PrepareProductOverviewModelsAsync(products, true, true, productThumbPictureSize)).ToList();
 
-        model = model.Take(5).ToList();
+        model = model.Take(6).ToList();
         return View(model);
     }
 }
